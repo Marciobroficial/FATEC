2bc217848c5445c0ff82560125585ba1f8a95498
MARCIO SANTOS <marciobroficial@gmail.com>
2020-04-01 05:43:47 -0300

    SUbindo 2 exerc

---
+++ Algoritimos e Lógistica de Programação/Comando de entrada e saída/exerc.02.py
@@ -0,0 +1,14 @@
+#Entrada de dados
+
+num01 = int(input ("insira o primeiro numero: "))
+num02 = int(input ("insira o segundo numero: "))
+num03 = int(input ("insira o terceiro numero: "))
+num04 = int(input ("insira o quarto numero: "))
+
+#Processamento
+
+conta = num01 + num02 + num03 + num04
+
+#Saida de dados
+
+print ("Soma Geral: ",conta)
