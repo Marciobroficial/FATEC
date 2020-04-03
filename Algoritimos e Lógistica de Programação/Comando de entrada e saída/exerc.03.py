e65fbdceb09058c22e3d2c8e697a00c48255b11a
MARCIO SANTOS <marciobroficial@gmail.com>
2020-04-03 04:19:30 -0300

    Create exerc.03.py

---
+++ Algoritimos e Lógistica de Programação/Comando de entrada e saída/exerc.03.py
@@ -0,0 +1,5 @@
+par01 = float(input('digita a primeira nota: '))
+par02 = float(input('digita a segunda nota: '))
+par03 = float(input('digita a terceira nota: '))
+soma = (par01 + par02 + par03) / 3
+print ('Media Final; ',soma)
