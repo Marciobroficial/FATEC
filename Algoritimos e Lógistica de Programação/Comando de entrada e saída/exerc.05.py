a0e3e80682dd5994b03a23666a36661133f94970
MARCIO SANTOS <marciobroficial@gmail.com>
2020-04-05 04:24:33 -0300

    exerc.05
    exerc.05

---
+++ Algoritimos e Lógistica de Programação/Comando de entrada e saída/exerc.05.py
@@ -0,0 +1,4 @@
+salario = float (input('qual o salário?: ' ))
+percentual = float (input('E qual o percentual para o aumento: '))
+aumento = salario * (percentual / 100)
+print ('O novo salário será de: ',soma)
