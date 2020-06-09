2d065e8bac4541ce505d4b626e830b842a97b408
MARCIO SANTOS <marciobroficial@gmail.com>
2020-06-09 10:12:53 -0300

    ok

--- Algoritimos e Lógistica de Programação/Estrutura de Repetição/ex.rep.12.py
+++ Algoritimos e Lógistica de Programação/Estrutura de Repetição/ex.rep.12.py
@@ -1,1 +1,3 @@
-12. Construir um algoritmo que, dado o valor de 20 preços de TV, determine e apresente o valor mais caro e mais barato entre eles.
+# 12. Construir um algoritmo que, dado o valor de 20 preços de TV,
+# determine e apresente o valor mais caro e mais barato entre eles.
+###################################################################
