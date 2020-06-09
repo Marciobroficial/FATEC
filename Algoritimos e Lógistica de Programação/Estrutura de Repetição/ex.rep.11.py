0ac0392d32b68542c13421cd9ac72e0defa1b35a
MARCIO SANTOS <marciobroficial@gmail.com>
2020-06-09 10:08:00 -0300

    ok
    ok

--- Algoritimos e Lógistica de Programação/Estrutura de Repetição/ex.rep.11.py
+++ Algoritimos e Lógistica de Programação/Estrutura de Repetição/ex.rep.11.py
@@ -1,5 +1,21 @@
 
+#
 # 11. Construir um algoritmo para calcular a média aritmética de preços de 5 produtos.
 
 ######################################################################################
 
+# soma = 0
+# c = 1
+# while c <= 5:
+#     p = float(input("Qual o preço do produto n° {} -> ".format(c)))
+#     soma += p
+#     c += 1
+# media = soma / 5
+# print("A média de preço dos produtos é R${:.2f}".format(media))
+
+soma = 0
+for c in range(1, 6):
+    p = float(input("Qual o preço do produto n° {} -> ".format(c)))
+    soma += p
+media = soma / 5
+print("A média de preço dos produtos é R$: {:.2f}".format(media))
